9c05c9ed96f1959078752abdb98027625520e231
Robin Hossen <hossenrobin215@gmail.com>
2026-06-16 16:21:10 +0600

    cart ar kaj 22.1,2,3,4

--- order/views.py
+++ order/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import GenericViewSet,ModelViewSet
+from rest_framework.mixins import CreateModelMixin,RetrieveModelMixin,DestroyModelMixin
+from order.models import Cart,CartItem
+from order.serializers import CartSerializer,CartItemSerializer
+
+
+class CartViewSet(CreateModelMixin,RetrieveModelMixin,DestroyModelMixin,GenericViewSet):
+    queryset=Cart.objects.all()
+    serializer_class=CartSerializer
+
+
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class=CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
+
+
+
