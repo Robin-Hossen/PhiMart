6c2a3a5ea209f5214d92bdf6db0085ae48845938
Robin Hossen <hossenrobin215@gmail.com>
2026-06-16 22:46:02 +0600

    setup joser and simple jwt 23.1

--- phi_mart/settings.py
+++ phi_mart/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'djoser',
     'api',
     'order',
     'users',
@@ -133,5 +134,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 REST_FRAMEWORK={
     'COERCE_DECIMAL_TO_STRING':False,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     
+}
+
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
 }
