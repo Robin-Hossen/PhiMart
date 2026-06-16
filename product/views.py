ffabb96c71fad927522c67fc522cf4cde76d7d05
Robin Hossen <hossenrobin215@gmail.com>
2026-06-16 12:06:51 +0600

    searchin and sorting 21.8

--- product/views.py
+++ product/views.py
@@ -7,14 +7,17 @@ from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from product.filters import ProductFilter
+from rest_framework.filters import SearchFilter,OrderingFilter
 
 
 class ProductViewSet(ModelViewSet):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
-    filter_backends=[DjangoFilterBackend]
+    filter_backends=[DjangoFilterBackend,SearchFilter,OrderingFilter]
     # filterset_fields=['category_id','price']
     filterset_class=ProductFilter
+    search_fields=['name','description','category__name']
+    ordering_fields=['price']
 
     # def get_queryset(self):
     #     queryset=Product.objects.all()
