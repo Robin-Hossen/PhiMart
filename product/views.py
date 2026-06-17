8457df708025b686cb7286c741e7a11617b729f6
Robin Hossen <hossenrobin215@gmail.com>
2026-06-17 15:51:27 +0600

    securing product review api 23.8

--- product/views.py
+++ product/views.py
@@ -11,6 +11,7 @@ from rest_framework.filters import SearchFilter,OrderingFilter
 from product.pagination import DefaultPagination
 from api.permissions import IsAdminOrReadOnly,FullDjangoModelPermission
 from rest_framework.permissions import DjangoModelPermissions
+from product.permissions import IsReviewAuthorOrReadonly
 
 class ProductViewSet(ModelViewSet):
     queryset=Product.objects.all()
@@ -42,12 +43,21 @@ class ProductViewSet(ModelViewSet):
 
 class CategoryViewSet(ModelViewSet):
     permission_classes=[IsAdminOrReadOnly]
-    
+
     queryset=Category.objects.annotate(product_count=Count('products')).all()
     serializer_class=CategorySerializer
 
+
+
 class ReviewViewSet(ModelViewSet):
     serializer_class=ReviewSerializer
+    permission_classes=[IsReviewAuthorOrReadonly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)    
 
     def get_queryset(self):
         return Review.objects.filter(product_id=self.kwargs['product_pk'])
