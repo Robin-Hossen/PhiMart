e6b83a6663e47c3ce6929975a8e76e0ebc02e4d9
Robin Hossen <hossenrobin215@gmail.com>
2026-06-18 09:26:00 +0600

    order create 24.4

--- order/views.py
+++ order/views.py
@@ -46,8 +46,10 @@ class OrderViewSet(ModelViewSet):
     def get_serializer_class(self):
         if self.request.method=='POST':
             return CreateOrderSerializer
-        
         return OrderSerializer
+    
+    def get_serializer_context(self):
+        return {'user_id':self.request.user.id}
 
     def get_queryset(self):
         if self.request.user.is_staff:
