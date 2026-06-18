065aee0737a62c401894ea26c4c202dfea26302f
Robin Hossen <hossenrobin215@gmail.com>
2026-06-18 15:00:39 +0600

    action decorator for updating order status 24.9

--- order/views.py
+++ order/views.py
@@ -47,23 +47,33 @@ class OrderViewSet(ModelViewSet):
     # permission_classes=[IsAuthenticated]
     http_method_names=['get','post','patch','delete','head','options']
 
-    @action(detail=True,methods=['post'],permission_classes=[IsAuthenticated])
+    @action(detail=True,methods=['post'])
     def cancel(self,request,pk=None):
         order=self.get_object()
         OrderService.cancel_order(order=order,user=request.user)
         return Response({'status':'Order canceled'})
+    
+    @action(detail=True,methods=['patch'])
+    def update_status(self, request,pk=None):
+        order=self.get_object()
+        serializer=UpdateOrderSerializer(order,data=request.data,partial=True)
+        serializer.save()
+        return Response({'status':f"Order status updated to {request.data['status']}" })
+    
+
+        
 
     def get_permissions(self):
-        if self.request.method=='DELETE':
+        if self.action in ['update_status','destroy']:
             return [IsAdminUser()]
         return [IsAuthenticated()]
 
     def get_serializer_class(self):
-        if self.request.method=='cancel':
+        if self.action=='cancel':
             return EmptySerializer
-        if self.request.method=='POST':
+        if self.action=='create':
             return CreateOrderSerializer
-        elif self.request.method=='PATCH':
+        elif self.action=='update_status':
             return UpdateOrderSerializer
         return OrderSerializer
     
