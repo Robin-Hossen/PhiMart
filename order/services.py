0a369f6defae5a2f9e18bf00e473ef4293fe3257
Robin Hossen <hossenrobin215@gmail.com>
2026-06-18 12:01:41 +0600

    implemented order canceling system 24.7

--- order/services.py
+++ order/services.py
@@ -1,8 +1,10 @@
 from order.models import Cart,CartItem,Order,OrderItem
 from django.db import transaction
+from rest_framework.exceptions import PermissionDenied,ValidationError
 
-@staticmethod
+
 class OrderService:
+    @staticmethod
     def create_order(user_id,cart_id):
         with transaction.atomic():#server pblm face na korar jonne
             cart=Cart.objects.get(pk=cart_id)
@@ -24,3 +26,17 @@ class OrderService:
             OrderItem.objects.bulk_create(order_items)
             cart.delete()
             return order
+    @staticmethod    
+    def cancel_order(order,user):   
+        if user.is_staff:
+            order.status=Order.CANCELED
+            order.save()
+            return order
+        if order.user != user:
+            raise PermissionDenied({'detail':"You can only cancel your own order"})
+        if order.status==Order.DELIVERED:
+            raise ValidationError({'detail':"You can not cancel an order"})
+        
+        order.status=Order.CANCELED
+        order.save()
+        return order
