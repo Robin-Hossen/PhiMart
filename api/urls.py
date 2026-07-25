c16cd336ef868707c5d1f008afaa782db8d4a4c2
Robin Hossen <hossenrobin215@gmail.com>
2026-07-26 00:10:58 +0600

    SSLcommerceZ payment initiate api added

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 from product.views import ProductViewSet,CategoryViewSet,ReviewViewSet,ProductImageViewset
 from rest_framework_nested import routers
-from order.views import CartViewSet,CartItemViewSet,OrderViewSet
+from order.views import CartViewSet,CartItemViewSet,OrderViewSet,initiate_payment
 
 # router=SimpleRouter()
 router=routers.DefaultRouter()
@@ -32,5 +32,6 @@ urlpatterns =[
     path('',include(cart_router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('payment/initiate/',initiate_payment,name="initiate-payment"),
     
 ]
